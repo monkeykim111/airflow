f22f0dc2f77c4b67f7c8f70c47276efb94baa73f
monkeyKim <ki102501@gmail.com>
2024-09-17 00:28:23 +0900

    task connection 오타수정

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -5,7 +5,7 @@ from airflow.operators.empty import EmptyOperator
 with DAG(
     dag_id="dags_conn_tast",
     schedule=None,
-    start_data=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     
