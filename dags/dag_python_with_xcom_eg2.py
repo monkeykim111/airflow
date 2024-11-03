a6b99a785f19d4939e6ee72691f0d4c28d002c66
monkeyKim <ki102501@gmail.com>
2024-11-03 15:41:19 +0900

    python with xcom 에러 fix

--- dags/dag_python_with_xcom_eg2.py
+++ dags/dag_python_with_xcom_eg2.py
@@ -9,24 +9,22 @@ with DAG(
     catchup=False
 ) as dag:
     @task(task_id='python_xcom_push_by_return')
-    def xcom_push_result():
+    def xcom_push_result(**kwargs):
         return 'Success'
     
     @task(task_id='python_xcom_pull_1')
-    def xcom_pull_1():
-        from airflow.decorators import get_current_context
-        context = get_current_context()
-        ti = context['ti']
+    def xcom_pull_1(**kwargs):
+        ti = kwargs['ti']
         value1 = ti.xcom_pull(task_id='python_xcom_push_by_return')
-        print(f'xcom_pull 메서드로 직접 찾은 리턴 값: {value1}')
+        print('xcom_pull 메서드로 직접 찾은 리턴 값:' + value1)
+
 
     @task(task_id='python_xcom_pull_2')
-    def xcom_pull_2(status):
-        print(f'함수 입력값으로 받은 값: {status}')
+    def xcom_pull_2(status, **kwargs):
+        print('함수 입력값으로 받은 값:', status)
 
-    # 태스크 호출 및 의존성 설정
+
     python_xcom_push_by_return = xcom_push_result()
-    python_xcom_pull_2 = xcom_pull_2(python_xcom_push_by_return)
-    
-    # python_xcom_push_by_return이 xcom_pull_1과 xcom_pull_2의 의존성이 되도록 설정
-    python_xcom_push_by_return >> [xcom_pull_1(), python_xcom_pull_2]
+    xcom_pull_2(python_xcom_push_by_return)
+    python_xcom_push_by_return >> xcom_pull_1()
+        
