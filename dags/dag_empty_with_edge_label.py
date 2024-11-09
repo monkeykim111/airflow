ccdd1ff105fb4d37eb058abf54aa314d60f28cef
monkeyKim <ki102501@gmail.com>
2024-11-09 13:38:11 +0900

    edge label test

---
+++ dags/dag_empty_with_edge_label.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.empty import EmptyOperator
+from airflow.decorators import task, task_group
+from airflow.utils.task_group import TaskGroup
+from airflow.utils.edgemodifier import Label
+
+
+with DAG(
+    dag_id="dag_python_with_edge_label",
+    schedule="10 9 * * *", 
+    start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    empty_1 = EmptyOperator(
+        task_id='empty_1'
+    )
+
+    empty_2 = EmptyOperator(
+        task_id='empty_2'
+    )
+
+    empty_1 >> Label('1과 2사이') >> empty_2
+
+    empty_3 = EmptyOperator(
+        task_id='empty_3'
+    )
+
+    empty_4 = EmptyOperator(
+        task_id='empty_4'
+    )
+
+    empty_5 = EmptyOperator(
+        task_id='empty_5'
+    )
+
+    empty_6 = EmptyOperator(
+        task_id='empty_6'
+    )
+
+    empty_2 >> Label('Start Branch') >> [empty_3, empty_4, empty_5] >> Label('End Branch') >> empty_6
