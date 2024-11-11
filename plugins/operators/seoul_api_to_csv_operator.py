26b3ca00b07d16fc3eda035b32406543a55902a3
monkeyKim <ki102501@gmail.com>
2024-11-12 01:18:41 +0900

    오타 수정

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -2,16 +2,16 @@ from airflow.models.baseoperator import BaseOperator
 from airflow.hooks.base import BaseHook
 import pandas as pd
 
-
 class SeoulApiToCsvOperator(BaseOperator):
     template_fields = ('endpoint', 'path', 'file_name', 'base_dt')
     
     def __init__(self, dataset_name, path, file_name, base_dt=None, **kwargs):
         super().__init__(**kwargs)
-        self.http_conn_id='openapi.seoul.go.kr',
-        self.path = path,
-        self.file_name = file_name,
-        self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json' + dataset_name,
+        # 쉼표 제거하여 tuple이 아닌 문자열로 설정
+        self.http_conn_id = 'openapi.seoul.go.kr'
+        self.path = path
+        self.file_name = file_name
+        self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json' + dataset_name
         self.base_dt = base_dt
 
     def execute(self, context):
@@ -37,10 +37,10 @@ class SeoulApiToCsvOperator(BaseOperator):
                 start_row = end_row + 1
                 end_row += 1000
 
+            # 경로 오타 수정
             if not os.path.exists(self.path):
-                os.system(f'mkdir -p {self.paht}')
-            total_row_df.to_csv(self.paht + '/' + self.file_name, encoding='utf-8', index=False)
-
+                os.makedirs(self.path)
+            total_row_df.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)
 
     def _call_api(self, base_url, start_row, end_row):
         import requests
@@ -55,12 +55,22 @@ class SeoulApiToCsvOperator(BaseOperator):
         request_url = f'{base_url}/{start_row}/{end_row}/'
         if self.base_dt is not None:
             request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
-        response = requests.get(request_url, headers)
-        contents = json.loads(response.text)
+        
+        response = requests.get(request_url, headers=headers)
+        
+        # 응답 확인 후 JSON 파싱
+        if response.status_code == 200 and response.text:
+            try:
+                contents = json.loads(response.text)
+            except json.JSONDecodeError:
+                self.log.error("Failed to parse JSON response.")
+                raise
 
-        key_name = list(contents.keys())[0]
-        row_data = contents.get(key_name).get('row')
-        row_df = pd.DataFrame(row_data)
+            key_name = list(contents.keys())[0]
+            row_data = contents.get(key_name).get('row')
+            row_df = pd.DataFrame(row_data)
 
-        return row_df
-
+            return row_df
+        else:
+            self.log.error("API response error with status code: %s", response.status_code)
+            raise Exception("API response error")
