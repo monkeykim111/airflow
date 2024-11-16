0bb54473783319bc85ab811a5f3083bd2e4fffe2
monkeyKim <ki102501@gmail.com>
2024-11-16 17:57:37 +0900

    dag 이름 안바꿈

--- dags/dag_python_with_postgres.py
+++ dags/dag_python_with_postgres.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 
 
 with DAG(
-    dag_id="dag_simple_http_operator",
+    dag_id="dag_python_with_postgres",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 3, 1, tz="Asia/Seoul"),
     catchup=False
