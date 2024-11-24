b6d9a77b055ebf8e94065c517a025a24dfc091a3
monkeyKim <ki102501@gmail.com>
2024-11-24 20:46:08 +0900

    test

--- dags/dag_check_logical_date.py
+++ dags/dag_check_logical_date.py
@@ -2,17 +2,18 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 import pendulum
 
-def check_context(context):
-    # Context 내 logical_date 확인
+def check_context(**kwargs):
+    # Context는 kwargs를 통해 접근 가능
+    context = kwargs
     if 'logical_date' in context:
-        print(f"[INFO] logical_date: {context['logical_date']}")
+        context['ti'].log.info(f"[INFO] logical_date: {context['logical_date']}")
     else:
-        print("[ERROR] logical_date not found in context!")
+        context['ti'].log.error("[ERROR] logical_date not found in context!")
 
 with DAG(
     dag_id='check_logical_date',
     schedule="0 13 * * *",  # 매일 13시에 실행
-    start_date=pendulum.datetime(2024, 11, 25, tz='Asia/Seoul'),
+    start_date=pendulum.datetime(2024, 11, 20, tz='Asia/Seoul'),
     catchup=False,
     default_args={'owner': 'airflow'}
 ) as dag:
