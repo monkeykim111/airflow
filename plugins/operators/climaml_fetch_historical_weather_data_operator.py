9a61e6297e885f94e1a4ee0e4aeb70ef6130e8e3
monkeyKim <ki102501@gmail.com>
2024-11-24 23:49:57 +0900

    template fields 다시 추가

--- plugins/operators/climaml_fetch_historical_weather_data_operator.py
+++ plugins/operators/climaml_fetch_historical_weather_data_operator.py
@@ -8,42 +8,32 @@ import numpy as np
 
 
 class ClimamlFetchHistoricalWeatherDataOperator(BaseOperator):
-    def __init__(self, conn_id, station_ids, **kwargs):
+    template_fields = ('start_date', 'end_date')
+
+    def __init__(self, conn_id, start_date, end_date, station_ids, **kwargs):
         super().__init__(**kwargs)
         self.conn_id = conn_id
+        self.start_date = start_date  
+        self.end_date = end_date 
         self.station_ids = station_ids
 
     def execute(self, context):
-        # Airflow context에서 logical_date 가져오기
-        logical_date = context['logical_date']
-        data_interval_start = context['data_interval_start']
-        data_interval_end = context['data_interval_end']
-        print(f"Logical Date: {logical_date}")
-        print(f"Data Interval Start: {data_interval_start}")
-        print(f"Data Interval End: {data_interval_end}")
-        start_date = logical_date.subtract(days=1).strftime('%Y-%m-%d')
-        end_date = start_date  # 동일한 날짜 사용
+        start_date = pendulum.parse(self.start_date)
+        end_date = pendulum.parse(self.end_date)
 
         # PostgreSQL 연결 설정
         postgres_hook = PostgresHook(postgres_conn_id=self.conn_id)
         engine = postgres_hook.get_sqlalchemy_engine()
 
         # API 기본 설정
-        api_key = Variable.get("data_go_kr")
+        api_key = Variable.get("data_go_kr")  # Airflow Variable에서 API 키 가져오기
         url = 'http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList'
 
-        current_start = pendulum.parse(start_date)
-        current_end = pendulum.parse(end_date)
+        # 데이터 요청 및 처리
+        current_start = start_date
 
-        request_count = 0
-        max_requests_per_day = 10000
-
-        while current_start <= current_end:
-            if request_count >= max_requests_per_day:
-                self.log.info("일일 최대 API 요청 수에 도달했습니다. 동작을 중단합니다.")
-                break
-
-            end_of_period = min(current_start.add(days=90), current_end)
+        while current_start <= end_date:
+            end_of_period = min(current_start.add(days=90), end_date)
 
             params_base = {
                 'serviceKey': api_key,
@@ -58,7 +48,6 @@ class ClimamlFetchHistoricalWeatherDataOperator(BaseOperator):
 
             try:
                 df = fetch_weather_data(params_base=params_base, station_ids=self.station_ids, url=url)
-                request_count += len(self.station_ids)
                 df = df.drop_duplicates()
                 df.replace("", np.nan, inplace=True)
                 df.to_sql('clima_ml_weather_data', engine, if_exists='append', index=False)
